a963d6e0c7457ffb799c6c0142badfdfea997280
Ronyeri Marinho <76921589+ronny-souza@users.noreply.github.com>
2024-05-11 12:21:28 -0300

    Delete benchmark directory

--- benchmark/src/main/java/br/com/rmsa/benchmark/exception/EmptyFileException.java
+++
@@ -1,8 +0,0 @@
-package br.com.rmsa.benchmark.exception;
-
-public class EmptyFileException extends Exception {
-
-    public EmptyFileException(String message) {
-        super(message);
-    }
-}
--- benchmark/src/main/java/br/com/rmsa/benchmark/repository/MetadataRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.rmsa.benchmark.repository;
-
-import br.com.rmsa.benchmark.model.Metadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetadataRepository extends JpaRepository<Metadata, Long> {
-}
